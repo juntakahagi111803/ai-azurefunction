e38ff61f681990dc6659b83f9020a6646a2e09fb
juntakahagi111803 <147367460+juntakahagi111803@users.noreply.github.com>
2025-01-13 15:07:27 +0900

    fix15

--- function_app.py
+++ function_app.py
@@ -1,27 +1,49 @@
+"""
+An Azure Function that interacts with Azure OpenAI and uses HTTP streaming. 
+"""
+
 import azure.functions as func
-import logging
 import openai
+from azurefunctions.extensions.http.fastapi import Request, StreamingResponse
+import asyncio
+import os
 
-
+# Azure Function App
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
-@app.route(route="http_trigger1")
-def http_trigger1(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+endpoint = os.environ["AZURE_OPEN_AI_ENDPOINT"]
+api_key = os.environ["AZURE_OPEN_AI_API_KEY"]
 
-    user_input = req.params.get('user_input')
-    if not user_input:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            user_input = req_body.get('user_input')
+# Azure Open AI
+deployment = os.environ["AZURE_OPEN_AI_DEPLOYMENT_MODEL"]
+temperature = 0.7
 
-    if user_input:
-        return func.HttpResponse("Hello, . This HTTP triggered function executed successfully!!!!!!!!!!!!!!")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+client = openai.AsyncAzureOpenAI(
+    azure_endpoint=endpoint,
+    api_key=api_key,
+    api_version="2023-09-01-preview"
+)
+
+# Get data from Azure Open AI
+async def stream_processor(response):
+    async for chunk in response:
+        if len(chunk.choices) > 0:
+            delta = chunk.choices[0].delta
+            if delta.content: # Get remaining generated response if applicable
+                await asyncio.sleep(0.1)
+                yield delta.content
+
+
+# HTTP streaming Azure Function
+@app.route(route="stream-cities", methods=[func.HttpMethod.GET])
+async def stream_openai_text(req: Request) -> StreamingResponse:
+    prompt = "List the 100 most populous cities in the United States."
+    azure_open_ai_response = await client.chat.completions.create(
+        model=deployment,
+        temperature=temperature,
+        max_tokens=1000,
+        messages=[{"role": "user", "content": prompt}],
+        stream=True
+    )
+
+    return StreamingResponse(stream_processor(azure_open_ai_response), media_type="text/event-stream")
